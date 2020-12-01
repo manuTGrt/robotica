234b78fc0ca4ee150ebb224de7e7eb422352abe2
manuTGrt <34089969+manuTGrt@users.noreply.github.com>
2020-12-01 01:31:21 +0100

    función mover robot v2 con mapa
    implementamos el dibujado del mapa que irá leyendo el robot de forma teórica para su posterior implementación con el robot real.

--- mapa_lectura_sonic.m
+++ mapa_lectura_sonic.m
@@ -7,8 +7,11 @@ crear_robot(0,0,0,0,SR_robot,SR_cabeza)
 
 distancia=5;
 mapa=[];
+mapa=load('mapa.dat');
 
 for alfa=0:0.01:pi/2
-    mover_robot_v2(0,0,0,alfa,SR_robot,SR_cabeza,distancia);
+    mapa=mover_robot_v2(-3,-3,pi/2,alfa,SR_robot,SR_cabeza,distancia,mapa);
     drawnow;
 end
+
+save('mapa.dat','mapa','-ascii');
--- mover_robot_v2.m
+++ mover_robot_v2.m
@@ -1,4 +1,4 @@
-function mover_robot_v2(x,y,theta,alfa,robot,cabeza,distancia)
+function mapa_out=mover_robot_v2(x,y,theta,alfa,robot,cabeza,distancia,mapa)
 %MOVER_ROBOT Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -10,7 +10,13 @@ robot.Matrix=M;
 M=makehgtform('translate',[2 0 0],'zrotate',alfa);
 cabeza.Matrix=M;
 
-mapa=robot.Matrix*cabeza.Matrix;
-mapa
+Mt=robot.Matrix*cabeza.Matrix;
+punto=Mt*[distancia 0 0 1]';
+
+axis([-10 10 -10 10]);
+
+mapa_out=[mapa; punto(1) punto(2)];
+
+d=animatedline(mapa_out(:,1),mapa_out(:,2),'Marker','*','LineStyle','none');
 end
 

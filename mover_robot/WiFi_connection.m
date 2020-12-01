911a65c63108095bdf52677e748467c199986c8f
manuTGrt <34089969+manuTGrt@users.noreply.github.com>
2020-12-01 18:12:19 +0100

    nueva carpeta del profe
    nueva carpeta del profesor

---
+++ mover_robot/WiFi_connection.m
@@ -0,0 +1,8 @@
+clear all
+clc
+%Crea el robot utilizando la conexión wiFi
+%Connect the Host Computer to an EV3 Brick Over a Wireless Network
+IPaddress='192.168.80.150';
+Robot_ID='00165814f48';
+mi_Robot  = legoev3('wifi',IPaddress,Robot_ID)
+%---------------------------------------
